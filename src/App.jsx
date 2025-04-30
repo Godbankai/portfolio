c6db169b623e398a263e419d4ff4bfa77ddb9ba7
Godbankai <134476125+Godbankai@users.noreply.github.com>
2025-05-01 00:57:13 +0530

    added vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,12 @@ import Projects from './components/Projects';
 import Skills from './components/Skills';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+import { Analytics } from "@vercel/analytics/react"
 
 const App = () => {
   return (
     <>
-    
+    <Analytics />
     <Navbar />
     <Hero />
     <About />
